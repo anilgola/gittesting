0206c983396d5647d95a50bbe5576eee12438470
anil <anil.gola@tothenew.com>
2022-06-30 14:14:46 +0530

    created feature/branch1 from main - updated

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -1,5 +1,6 @@
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("created feature/branch1 from main");;
+        System.out.println("created feature/branch1 from main");
+        System.out.println("created feature/branch1 from main - updated");
     }
 }
